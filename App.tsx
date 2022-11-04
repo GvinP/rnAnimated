d26b82a56b8a0899e7f51fb2024b44aa2974acf4
alexey kudriashov <gvinpin72@gmail.com>
2022-11-04 11:41:30 +0300

    The basics of PanGestureHandler with React Native Reanimated 2

--- App.tsx
+++ App.tsx
@@ -1,11 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useCallback, useEffect } from "react";
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+  interpolate,
+  Extrapolate,
+  withTiming,
+  withRepeat,
+  useAnimatedGestureHandler,
+} from "react-native-reanimated";
+
+const SIZE = 100;
+const RADIUS = SIZE * 1.8;
+
+type ContextType = {
+  translateX: number;
+  translateY: number;
+};
 
 export default function App() {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const panGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    ContextType
+  >({
+    onStart: (event, context) => {
+      context.translateX = translateX.value;
+      context.translateY = translateY.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = event.translationX + context.translateX;
+      translateY.value = event.translationY + context.translateY;
+    },
+    onEnd: (event) => {
+      const distance = Math.sqrt(translateX.value ** 2 + translateY.value ** 2);
+      if (distance < RADIUS+SIZE/2) {
+        translateX.value = withSpring(0);
+        translateY.value = withSpring(0);
+      }
+    },
+  });
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+      ],
+    };
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.circle}>
+        <PanGestureHandler onGestureEvent={panGestureEvent}>
+          <Animated.View style={[styles.square, rStyle]} />
+        </PanGestureHandler>
+      </View>
     </View>
   );
 }
@@ -13,8 +74,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ddd",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  square: {
+    height: 100,
+    width: 100,
+    borderRadius: 25,
+    backgroundColor: "teal",
+  },
+  circle: {
+    height: RADIUS * 2,
+    width: RADIUS * 2,
+    borderRadius: RADIUS,
+    borderColor: "teal",
+    borderWidth: 3,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
