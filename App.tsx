a5c5bcfd499ab09a2ebaed95d3fb87e90b01dad4
alexey kudriashov <gvinpin72@gmail.com>
2022-11-15 12:12:24 +0300

    react native maps

--- App.tsx
+++ App.tsx
@@ -2,14 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import Layout from "./components/Layout ";
+import Maps from "./components/Maps";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={styles.container}>
         <StatusBar style="auto" />
-        <Layout />
+        <Maps />
       </View>
     </GestureHandlerRootView>
   );
---
+++ components/Maps.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import MapView, { Marker, PROVIDER_GOOGLE, Callout } from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+
+const Maps = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        // provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 56.838938,
+          longitude: 60.603195,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: 56.838938,
+            longitude: 60.603195,
+          }}
+          pinColor={"green"}
+        >
+            <Callout>
+                <View style={{width: 200, height: 150, borderRadius: 20, backgroundColor: "#cecece", margin: 0}}>
+                    <Text>
+                    Билеты в театр
+                    </Text>
+                </View>
+            </Callout>
+        </Marker>
+      </MapView>
+    </View>
+  );
+};
+
+export default Maps;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
