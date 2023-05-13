ec959eb3283b3acdbd1bada692449bce16c39fae
kudryashovalexey <gvinpin72@gmail.com>
2023-05-13 16:05:13 +0300

    morthing shapes

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import Accordion from "./components/Accordion";
+import ShapeMorphing from "./components/ShapeMorthing/ShapeMorphing";
 
 export default function App() {
   return (
@@ -11,7 +11,7 @@ export default function App() {
       <SafeAreaProvider style={{ flex: 1 }}>
         <View style={styles.container}>
           <StatusBar style="auto" />
-          <Accordion />
+          <ShapeMorphing />
         </View>
       </SafeAreaProvider>
     </GestureHandlerRootView>
